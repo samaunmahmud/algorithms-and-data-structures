2990c14950204855393266048c96901e89a92a30
samaunmahmud <samaunmahmud9@gmail.com>
2026-08-13 23:54:18 +0100

    Implementing the Bubble sorting Algorithm

---
+++ src/SortingAlgorithm/Main.java
@@ -0,0 +1,13 @@
+package SortingAlgorithm;
+
+import static SortingAlgorithm.BubbleSorting.bubbleSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 2, 6, 8, 4, 7, 2, 6, 3};
+
+        bubbleSort(arr);
+    }
+}
---
+++ src/Trees/AVLTree.java
@@ -0,0 +1,45 @@
+//package Trees;
+//
+//public class AVLTree {
+//
+//    private class Node {
+//        Node value;
+//        Node left;
+//        Node right;
+//        Node(int value){
+//            this.value = new Node(value);
+//        }
+//    }
+//
+//    Node root = null;
+//
+//
+//    Node current = null;
+//
+//
+//
+//
+//    private void insert(int value, Node root){
+//
+//        if(root == null){
+//            root = new Node(value);
+//            current = root;
+//
+//        }
+//
+//        if(value  < root.value){
+//            insert(value, root.left);
+//
+//        }else if(value> root.value){
+//            insert(value, root.right);
+//        }
+//
+//
+//
+//
+//
+//    }
+//
+//
+//
+//}
