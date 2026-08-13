11fd4ca91bfbaf51c80811dcfd4cd9f9307c6271
samaunmahmud <samaunmahmud9@gmail.com>
2026-08-14 00:46:35 +0100

    Implemented the Selectin sorting Algorithm

--- src/SortingAlgorithm/Main.java
+++ src/SortingAlgorithm/Main.java
@@ -1,13 +1,19 @@
 package SortingAlgorithm;
 
 import static SortingAlgorithm.BubbleSorting.bubbleSort;
+import static SortingAlgorithm.SelectionSorting.selectionSort;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] arr = {4, 2, 6, 8, 4, 7, 2, 6, 3};
+        int[] arr = {6,4,5,2,4,7,3,7,2,9,2,1,5,8,4,3,5,7};
 
-        bubbleSort(arr);
+//        bubbleSort(arr);
+
+        selectionSort(arr);
+
+
     }
 }
---
+++ src/SortingAlgorithm/SelectionSorting.java
@@ -0,0 +1,34 @@
+package SortingAlgorithm;
+
+import java.util.Arrays;
+
+public class SelectionSorting {
+
+
+
+    public static void selectionSort(int[] arr){
+        for(int i =0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    swap(arr,i,j);
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+        for(int k : arr){
+            System.out.print(k+" ");
+        }
+    }
+
+
+    public static void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+}
