1dd51e7da997dc4ac694bcc9ef9eff4ab620a0f2
samaunmahmud <samaunmahmud9@gmail.com>
2026-08-16 15:56:20 +0100

    Built the reverse Words Algorithm , i used the StringBuilder , append, split

--- src/StringManipulation/Main.java
+++ src/StringManipulation/Main.java
@@ -5,14 +5,19 @@ public class Main {
 
     public static void main(String[] args){
 
-        String s = "aEiOu";
+        String s = "Me/I love you";
+
 
 //        int number = CountVowels.countVowels(s);
 //        System.out.println("Number of Vowles is : "+number);
 
         ReverseString reverseString = new ReverseString();
 
-        String st = reverseString.reverseString(s);
+//        String st = reverseString.reverseString(s);
+
+        ReverseWords reverseWords = new ReverseWords();
+
+        String st = reverseWords.reverseWords(s);
         System.out.println(st);
 
 
---
+++ src/StringManipulation/ReverseWords.java
@@ -0,0 +1,15 @@
+package StringManipulation;
+
+public class ReverseWords {
+
+    public String reverseWords(String s){
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]).append(" ");
+
+        }
+        return sb.toString();
+    }
+}
